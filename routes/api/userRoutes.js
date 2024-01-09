572a3625f10371cb3114c3d8b6389af28f5746eb
Jaysahhn <jasonmason96@yahoo.com>
2024-01-08 22:18:04 -0600

    modified file in api folder

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+
+const {
+    createUser,
+    getUsers,
+    getSingleUser,
+    deleteUser,
+    editUser,
+    addFriend,
+    deleteFriend,
+} = require('../../controllers/userController.js');
+
+router.route('/').get(getUsers).post(createUser);
+
+router.route('/:userId').get(getSingleUser);
+
+router.route('/:userId').delete(deleteUser);
+
+router.route('/:userId').put(editUser);
+
+router.route('/:userId/friends').post(addFriend);
+
+router.route('/:userId/friends').delete(deleteFriend);
+
+module.exports = router; 
