672ecce3a6f4ce73fcfcfad9219cce3356bb0c17
Jaysahhn <jasonmason96@yahoo.com>
2024-01-08 22:52:38 -0600

    modified connection.js to connect to mongodo

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
 const { connect, connection } = require('mongoose');
 
-connect('mongodb://localhost:27017/SocialNetworkAPI');
+connect('mongodb://127.0.0.1:27017/SocialNetworkAPI');
 
 module.exports = connection; 
