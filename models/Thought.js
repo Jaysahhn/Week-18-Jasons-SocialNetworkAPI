62c40187070f6a1c4519c586eae1bd124054ea8e
Jaysahhn <jasonmason96@yahoo.com>
2024-01-08 22:21:26 -0600

    modified file structure

--- models/Thought.js
+++ models/Thought.js
@@ -1,39 +1,61 @@
-const mongoose = require('mongoose');
-const reactionSchema = require('./Reaction');
+const { Schema, Types, model } = require('mongoose');
 
-const thoughtSchema = new mongoose.Schema({
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+const thoughtSchema = new Schema({
     thoughtText: {
         type: String,
         required: true,
-        minlength: 1,
-        maxlength: 280
+        minLength: 1,
+        maxLength: 280
     },
     createdAt: {
         type: Date,
         default: Date.now,
-        get: (timestamp) => new Date(timestamp).toISOString()
     },
     username: {
         type: String,
         required: true
     },
-    reactions: [reactionSchema],
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
+    reactions: [reactionSchema]
+},
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
     }
-}, {
-    toJSON: {
-        virtuals: true,
-        getters: true
-    }
-});
+);
 
 thoughtSchema.virtual('reactionCount').get(function () {
     return this.reactions.length;
 });
 
-const Thought = mongoose.model('Thought', thoughtSchema);
-
-module.exports = Thought;
+const Thought = model('Thought', thoughtSchema);
+module.exports = Thought; 
