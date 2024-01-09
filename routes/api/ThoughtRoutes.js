3655f7d772ec98ae4d0afa187c15903e1f61657d
Jaysahhn <jasonmason96@yahoo.com>
2024-01-08 22:17:12 -0600

    modified files in api folder

---
+++ routes/api/ThoughtRoutes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+
+const {
+    createThought,
+    getThought,
+    getSingleThought,
+    deleteThought,
+    editThought,
+    addReaction,
+    deleteReaction,
+} = require('../../controllers/thoughtController.js');
+
+router.route('/').get(getThought).post(createThought);
+
+router.route('/:thoughtId').get(getSingleThought);
+
+router.route('/:thoughtId').delete(deleteThought);
+
+router.route('/:thoughtId').put(editThought);
+
+router.route('/:thoughtId/reactions').post(addReaction);
+
+router.route('/:thoughtId/reactions').delete(deleteReaction);
+
+module.exports = router; 
